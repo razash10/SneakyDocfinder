f8d5b77ca448ec4fd3fd686880e9fa6283218e26
razas <razash10@gmail.com>
2025-10-07 23:48:37 +0300

    Updated DB_NAME; Fixed common/db.js

--- common/db.js
+++ common/db.js
@@ -1,10 +1,11 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const config = require('./config')
+const config = require("./config");
 
 mongoose.connect(config.mongoUrl, {
-  useNewUrlParser: true, 
-  dbName: config.dbName
-})
-mongoose.Promise = global.Promise
-mongoose.connection.on('error', (error) => console.error('MongoDB connection error:', error))
+  dbName: config.dbName,
+});
+mongoose.Promise = global.Promise;
+mongoose.connection.on("error", (error) =>
+  console.error("MongoDB connection error:", error)
+);
