35314be33a74222fa4475373af614ff74c8af4d6
razas <razash10@gmail.com>
2025-10-10 13:42:11 +0300

    Updated structure and runs on docker

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,8 @@ ARG service_src
 WORKDIR /usr/src/app
 COPY package*.json ./
 RUN npm install
+
 COPY ${service_src} ./service/src
-COPY ./common ./common
+COPY ./src/common ./service/common
+
 CMD [ "npm", "run", "start" ]
---
+++ src/crawler/index.js
@@ -0,0 +1,57 @@
+const crypto = require("crypto");
+
+const { fetchPage } = require("./request");
+const { parseHTML } = require("./parser");
+const config = require("./config");
+const queue = require("../common/queue");
+
+const createHash = (string) =>
+  crypto.createHash("md5").update(string).digest("hex");
+
+const subscribeToWorkingQueue = async () => {
+  try {
+    const consumeEmmitter = await queue.consume(
+      config.workerQueue,
+      false,
+      true,
+      1
+    );
+
+    consumeEmmitter.on("data", async (message, ack) => {
+      const { url, selector, chatId } = JSON.parse(message);
+      const page = await fetchPage(url).catch(async (error) => {
+        const errorResponse = {
+          chatId,
+          error: `${error.name}: ${error.message}`,
+        };
+        await queue.produce(
+          config.comparatorQueue,
+          JSON.stringify(errorResponse)
+        );
+        ack();
+        throw error;
+      });
+      const parsedString = parseHTML(page, selector);
+      const hash = createHash(parsedString);
+      const response = { chatId, hash };
+      await queue.produce(config.comparatorQueue, JSON.stringify(response));
+      ack();
+    });
+
+    consumeEmmitter.on("error", (error) => console.error(error));
+
+    return true;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+};
+
+const intervalId = setInterval(() => {
+  subscribeToWorkingQueue().then((isConnected) => {
+    if (isConnected) {
+      clearInterval(intervalId);
+      console.log("Crawler is up");
+    }
+  });
+}, config.rabbitReconnectInterval);
